11d49d9272bca9d698cb7b4dba7140dfbda4a5a6
unknown <juan.bueno01@correo.usa.edu.co>
2020-10-24 12:26:48 -0500

    dire

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ from mysql.connector import errorcode
 import cgi
 data = cgi.FieldStorage()
 Usuario =data.getvalue('Usuario')
-Contra =data.getvalue('Contra')
+Nombre =data.getvalue('Nombre')
 try:
   cnx = mysql.connector.connect(user='sebastian', password = 'Holasebas99.', database='arqui', host='127.0.0.1')
 except mysql.connector.Error as err:
@@ -26,11 +26,11 @@ else:
     if usuariob:
      for i in usuariob:
       usuario=i[2]
-      contra=i[0]
+      nombre=i[0]
     else:
      print('<h1> Fallo </h1>')
 
-    if 	Contra == contra and Usuario == usuario:
+    if 	Nombre == nombre and Usuario == usuario:
      print('<script> location.href="/ProyectoArquitectura/Login.html";</script>')
     else:
      print('<h1> Fallo </h1>')
