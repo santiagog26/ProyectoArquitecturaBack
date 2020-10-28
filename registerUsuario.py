88c12930908953a9f7a6331efb9b18f051c81070
unknown <juan.bueno01@correo.usa.edu.co>
2020-10-27 21:44:18 -0500

    adelanto

--- registerUsuario.py
+++ registerUsuario.py
@@ -15,7 +15,7 @@ try:
   cnx = mysql.connector.connect(user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
 except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
+    print("Something is wrong with you")
   elif err.errno == errorcode.ER_BAD_DB_ERROR:
     print("Database does not exist")
   else:
@@ -24,14 +24,11 @@ else:
     cur = cnx.cursor()
     print('Content-Type: text/html')
     print('')
-    #sql = ("insert into usuario values ('{}',SHA('{}'),'{}','{}','{}','{}')".format(Documento,Contraseña,Nombre,Apellido,Correo,Telefono))
-    #cur.execute(sql)
-    print('<h1> Fallo {} </h1>'.format(Documento))
-    print('<h1> Fallo {} </h1>'.format(Contraseña))
-    print('<h1> Fallo {} </h1>'.format(Nombre))
-    print('<h1> Fallo {} </h1>'.format(Apellido))
-    print('<h1> Fallo {} </h1>'.format(Correo))
-    print('<h1> Fallo {} </h1>'.format(Telefono))
+
+    sql = ("insert into usuario values ('{}',SHA('{}'),'{}','{}','{}','{}')".format(Documento,Contraseña,Nombre,Apellido,Correo,Telefono))
+    cur.execute(sql)
     cnx.commit()
+
 cnx.close()
+
 
