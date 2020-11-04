79e0bec637565cfff444f7747f9e623cb788a5a3
unknown <juan.bueno01@correo.usa.edu.co>
2020-11-03 22:06:02 -0500

    arreglo

---
+++ domiciliario.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import mysql.connector
+from mysql.connector import errorcode
+import cgi
+data = cgi.FieldStorage()
+Documento =data.getvalue('documentodo')
+Numero_pedido =data.getvalue('numero_pedido')
+
+try:
+  cnx = mysql.connector.connect(user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with you")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+    cur = cnx.cursor()
+    print('Content-Type: text/html')
+    print('')
+    sql = ("update pedido set empaquetado=(select rol from empacador where documento_empacador='{}') where numero_orden='{}';".format(Documento,Numero_pedido))
+    cur.execute(sql)
+    print('<script>alert("pedido empacado y actualizado ")</script>')
+    print('<script> location.href="/ProyectoArquitectura/menu.html";</script>')
+    cnx.commit()
+cnx.close()
