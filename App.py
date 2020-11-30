c1552ed13510929ba4f9b5092f08286c6365c2d7
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-11-30 10:32:21 -0500

    a

--- App.py
+++ App.py
@@ -24,30 +24,29 @@ def hellos():
 def mostrar_pedidos():
 	print('Hello')
 	try:
-		cnx = mysql.connector.connect(
-            user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
-    except mysql.connector.Error as err:
-    	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    		print("Something is wrong with your user name or password")
-    	elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    		print("Database does not exist")
-    	else:
-    		print(err)
-    else:
-    	cur = cnx.cursor()
-    	sql = ("select * from pedido")
-    	cur.execute(sql)
-    	row = cur.fetchall()
-    	lista = list()
-    	for i in row:
-    		numero_orden = i[0]
-    		fecha = i[1]
-    		cliente_documento = i[2]
-    		empaquetado = i[3]
-    		domicilio = i[4]
-    		vendedor = i[5]
-    		pedido = {'numero_orden': numero_orden, 'fecha': fecha, 'cliente_documento': cliente_documento, 'empaquetado': empaquetado, 'domicilio': domicilio, 'vendedor': vendedor}
-    		lista.append(pedido)
+		cnx = mysql.connector.connect(user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Something is wrong with your user name or password")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database does not exist")
+		else:
+			print(err)
+	else:
+		cur = cnx.cursor()
+		sql = ("select * from pedido")
+		cur.execute(sql)
+		row = cur.fetchall()
+		lista = list()
+		for i in row:
+			numero_orden = i[0]
+			fecha = i[1]
+			cliente_documento = i[2]
+			empaquetado = i[3]
+			domicilio = i[4]
+			vendedor = i[5]
+			pedido = {'numero_orden': numero_orden, 'fecha': fecha, 'cliente_documento': cliente_documento, 'empaquetado': empaquetado, 'domicilio': domicilio, 'vendedor': vendedor}
+			lista.append(pedido)
     	return jsonify(results=lista)
     cnx.close()	
 
