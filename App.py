f3ac772781b158ae667c151f112ad0ac3e157f31
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-11-29 23:24:37 -0500

    Flask get_usuario

--- App.py
+++ App.py
@@ -1,27 +1,74 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
-from mysql.connector import errorcode
 import mysql.connector
+from mysql.connector import errorcode
+from flask import Flask, jsonify
+from flask_cors import CORS
+import json
 
+# configuration
+DEBUG = True
+
+# instantiate the app
 app = Flask(__name__)
 
-try:
-  cnx = mysql.connector.connect(user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with you")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/')
-def Index():
-	return 'Hello world'
+def hellos():
+    return "Hello world"
 
-@app.route('/add_usuario')
-def add_usuario():
-	return render_template('home.html')
+@app.route('/get_usuarios', methods=['GET'])
+def mostrar_productos():
+	try:
+	    cnx = mysql.connector.connect(user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
+    except mysql.connector.Error as err:
+	    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		    print("Something is wrong with your user name or password")
+	    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		    print("Database does not exist")
+	    else:
+		    print(err)
+	else:
+		cur = cnx.cursor()
+		sql = ("select* from usuario")
+		cur.execute(sql)
+		row = cur.fetchall()
+		lista = list()
+		for i in row:
+			documento = i[0]
+			contrasena = i[1]
+			nombre = i[2]
+			apellido = i[3]
+			correo = i[4]
+			telefono = i[5]
+			persona = {'Documento': documento,
+			'Contraseña': contrasena,
+            'Nombre': nombre,
+            'Apellido': apellido,
+            'Correo': correo,
+            'Telefono': telefono}
+            lista.append(persona)
+        return jsonify(results=lista)
+    cnx.close()	
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+
+@app.route('/productosMO', methods=['PUT'])
+def modificar_productos():
+    return "Modificando producto uwu"
+
+
+@app.route('/productosB', methods=['DELETE'])
+def borrar_productos():
+    return "borrando producto unu"
+
+
+@app.route('/productosC', methods=['POST'])
+def crear_productos():
+    return "creando producto ewe"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
