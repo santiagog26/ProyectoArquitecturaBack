0407c814ce39fe13ba59dd38890bd925213c633e
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-11-29 23:44:31 -0500

    Error indent

--- App.py
+++ App.py
@@ -22,15 +22,17 @@ def hellos():
 
 @app.route('/get_usuarios', methods=['GET'])
 def mostrar_productos():
-	try:
-	    cnx = mysql.connector.connect(user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
+def mostrar_usuarios():
+    try:
+        cnx = mysql.connector.connect(
+            user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
     except mysql.connector.Error as err:
-	    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-		    print("Something is wrong with your user name or password")
-	    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-		    print("Database does not exist")
-	    else:
-		    print(err)
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
 	else:
 		cur = cnx.cursor()
 		sql = ("select* from usuario")
