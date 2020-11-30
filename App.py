7b494b55e7ed8a2f4f6f105beb3a45df48c06927
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-11-29 23:47:47 -0500

    a

--- App.py
+++ App.py
@@ -27,11 +27,11 @@ def mostrar_usuarios():
             user='sebastian', password = 'Holasebas99.', database='proyecto', host='127.0.0.1')
     except mysql.connector.Error as err:
     	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
+    		print("Something is wrong with your user name or password")
+    	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    		print("Database does not exist")
+    	else:
+    		print(err)
     else:
     	cur = cnx.cursor()
 		sql = ("select* from usuario")
